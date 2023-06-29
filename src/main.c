1dde60e46b4a87695db57d0858dc14beed30d4ee
Victor Rakotondranoro <adrianoviciiiz@gmail.com>
2023-06-28 21:50:18 -0500

    keyboard driver implemented The infinite loop in main() reads the keyboard input and displays the ascii value on the screen

--- src/main.c
+++ src/main.c
@@ -12,6 +12,11 @@ uint8_t inb (uint16_t _port) {
     return rv;
 }
 
+
+void outb (uint16_t _port, uint8_t val) {
+    __asm__ __volatile__ ("outb %0, %1" : : "a" (val), "dN" (_port) );
+}
+
 void main() {
     unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
     const unsigned char color = 7; // gray text on black background
@@ -19,10 +24,15 @@ void main() {
     esp_printf(putc, "hello world");
 
     while(1) {
+	// read from port 0x64
         uint8_t status = inb(0x64);
-
-        if(status & 1) {
-            uint8_t scancode = inb(0x60);
-        }
+	if(status & 1){
+	   uint8_t scancode = inb(0x60);
+           if(keyboard_map[scancode] >= 1 && keyboard_map[scancode]!=0){
+	   	if((scancode & 0x80) == 0){ 
+	   	   esp_printf(putc, "%c", keyboard_map[scancode]);
+		}
+	   }
+	}
     }
 }
